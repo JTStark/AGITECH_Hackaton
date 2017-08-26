218436fe5b58149575960d2779551d06bdf80482
MrStrings <phstring95@hotmail.com>
2017-08-26 18:36:43 -0300

    simple web server

--- chat_api/chat.py
+++ chat_api/chat.py
@@ -1,44 +1,5 @@
-"""
-This bot listens to port 5002 for incoming connections from Facebook. It takes
-in any messages that the bot receives and echos it back.
-"""
-from flask import Flask, request
-from pymessenger.bot import Bot
 from os import environ
+from flask import Flask
 
 app = Flask(__name__)
-
-ACCESS_TOKEN = "41d2af30c9e81e3243cdbe58d73bc9bf"
-VERIFY_TOKEN = "test_token"
-bot = Bot(ACCESS_TOKEN)
-
-
-
-@app.route("/", methods=['GET', 'POST'])
-def hello():
-    if request.method == 'GET':
-        if request.args.get("hub.verify_token") == VERIFY_TOKEN:
-            return request.args.get("hub.challenge")
-        else:
-            return 'Invalid verification token'
-
-    if request.method == 'POST':
-        output = request.get_json()
-        for event in output['entry']:
-            messaging = event['messaging']
-            for x in messaging:
-                if x.get('message'):
-                    recipient_id = x['sender']['id']
-                    if x['message'].get('text'):
-                        message = x['message']['text']
-                        bot.send_text_message(recipient_id, message)
-                    if x['message'].get('attachments'):
-                        for att in x['message'].get('attachments'):
-                            bot.send_attachment_url(recipient_id, att['type'], att['payload']['url'])
-                else:
-                    pass
-        return "Success"
-
-
-if __name__ == "__main__":
-    app.run(port=environ.get('PORT'), debug=True)
+app.run(environ.get('PORT'))
