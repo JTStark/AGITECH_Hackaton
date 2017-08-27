f76ca42c8f2878a1c9be61a6e6e9dcaa7c164ff7
MrStrings <phstring95@hotmail.com>
2017-08-27 00:52:14 -0300

    testing apiai

--- internal_processing/apiai_get.py
+++ internal_processing/apiai_get.py
@@ -3,10 +3,13 @@ import apiai
 CLIENT_ACCESS_TOKEN = '0b41530420854cd3a671a41a46553862'
 
 def parse_message(message):
-	conn = apiai.ApiAI(CLIENT_ACCESS_TOKEN)
+	conn = apiai.ApiAI('0b41530420854cd3a671a41a46553862')
 	request = ai.text_request()
 	request.lang = 'pt-BR'    #optional, default value equal en
 	request.session_id = "<SESSION ID, UNIQUE FOR EACH USER>"
 	request.query = message
 	response = request.getresponse()
 	return response
+
+
+parse_message("transfere")
